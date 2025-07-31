21d6815450b6f40bae27823b43f36fad3c082c6f
padhmasri84 <padhmasri7002@gmail.com>
2025-07-31 15:23:02 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,20 @@
+print("ATM")
+print("insert the card")
+data1="english"
+print("choose language:",data1)
+print("widthdraw")
+data2="1234"
+print("enter the pin:",data2)
+data3="1000"
+print("enter the amount:",data3)
+print("get the cash and receipt")
+print("get back the card")
+
+
+
+
+
+
+
+
+         
